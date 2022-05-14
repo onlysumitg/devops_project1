2a859990bbc89d0426b5f93954be3d4a5dec09f5
sumit <sumit@Sumit-001.attlocal.net>
2022-05-14 11:50:32 -0500

    pipeline step - build docker image

--- achistar/settings.py
+++ achistar/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = 'django-insecure-&gqa6y!ow=+g$ed#wqa^&ogxu(v%=m)u@to9s0o_a=ehmv4do$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1'
+]
 
 
 # Application definition
